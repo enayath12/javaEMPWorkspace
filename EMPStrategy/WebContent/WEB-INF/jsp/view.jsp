10203c60b47b9e799dd5b6a6f0345a673d42c11f
Your Name <you@example.com>
2019-09-23 23:51:25 +0530

    project name changed from TKD to sql creation

---
+++ EMPStrategy/WebContent/WEB-INF/jsp/view.jsp
@@ -0,0 +1,7 @@
+Hiiiiiiiiiiiiiiiiiiii
+
+<%@page import="java.io.FileInputStream"%>
+<%@page import="java.io.File"%>
+<form action="${pageContext.request.contextPath}/EMPStrategy" method="post" enctype="multipart/form-data">
+	<input type="submit" name="button1" value="EMP Strategy" />
+</form>
---
+++ EMPStrategy/src/com/tkd/EMP/EMPStrategy.java
@@ -0,0 +1,33 @@
+package com.tkd.EMP;
+
+
+	import java.io.BufferedReader;
+	import java.io.File;
+	import java.io.FileInputStream;
+	import java.io.FileNotFoundException;
+	import java.io.FileReader;
+	import java.io.IOException;
+	import java.util.Collections;
+	import java.util.Comparator;
+	import java.util.HashMap;
+	import java.util.LinkedHashMap;
+	import java.util.LinkedList;
+	import java.util.List;
+	import java.util.Map;
+
+	import javax.servlet.ServletException;
+	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+
+
+	@WebServlet("/EMPStrategy")
+	public class EMPStrategy extends HttpServlet {
+		private final String UPLOAD_DIRECTORY = "D:/Strategy testing environment/Workspace/TKD/TEMP tkd folder";
+		
+		 public static void main(String[] args) throws FileNotFoundException, IOException {
+			 
+		 }
+		 
+	}
